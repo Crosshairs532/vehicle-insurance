05ef18ecd6e09cb610d698b221b2b0ee576f1b99
Crosshairs532 <samsularefin532@gmail.com>
2025-10-10 17:31:31 +0600

    Data Transformation DONE

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -1,3 +1,4 @@
+from src.components.data_transformation import DataTransformation
 from src.components.data_validation import DataValidation
 from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from src.exception import CustomException
@@ -39,11 +40,27 @@ class TrainPipeline:
             return data_validation_artifact
         except Exception as e:
             raise CustomException(e, sys) from e
+
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise CustomException(e, sys)
+
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             logger.info('=============== Data Ingestion Completed =================')
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             logger.info('=============== Data Validation Completed =================')
+            data_transformation_artifact = self.start_data_transformation(
+            data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            logger.info('=============== Data Transformation Completed =================')
+
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -1,12 +1,56 @@
+import dill
 import yaml
 import sys
 from src.exception import CustomException
-
+from src.logger import logger
+import os
+import pandas as pd
+import numpy as np
 
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
 
+    except Exception as e:
+        raise CustomException(e, sys) from e
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+def save_object(file_path: str, obj: object) -> None:
+    logger.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logger.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
+def save_numpy_array_data(file_path: str, array: np.array):
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys) from e
